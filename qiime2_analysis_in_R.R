41832346a8632ce274c1f0d0354653a6685f0b18
LeungHK <89854896+LeungHK@users.noreply.github.com>
2022-05-25 16:45:29 +0800

    Update qiime2_analysis_in_R.R

--- qiime2_analysis_in_R.R
+++ qiime2_analysis_in_R.R
@@ -1,1 +1,26 @@
+###export qiime2 feature_table.qza into feature-table.biom format, to be imported into R via phlyoseq
 
+#install.packages("installr")
+#library("installr")
+#updateR()
+
+if(!requireNamespace("BiocManager")){
+  install.packages("BiocManager")
+}
+BiocManager::install("phyloseq")
+
+#source('http://bioconductor.org/biocLite.R')
+#biocLite('phyloseq')
+
+#install.packages("phyloseq")
+library("phyloseq")
+?import
+?import_qiime
+
+install.packages("vegan")
+library("vegan")
+
+getwd()
+setwd("path_to_directory")
+
+feature_table <- import_biom("feature-table.biom")
